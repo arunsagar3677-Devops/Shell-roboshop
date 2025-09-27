b4ca60f05356e3d2222ccee9233933b823899d4d
arunsagar3677-Devops <you@example.com>
2025-09-28 00:05:04 +0530

    sent msg

--- 02-conversion.sh
+++ 02-conversion.sh
@@ -1,4 +1,6 @@
 #!/bin/bash
 
-echo "Ramesh:: Hi Suresh, how are you?"
-echo :Suresh:: Hi i am good learing shell scrpting
+echo "Ramesh:: Hi Suresh, How are you?"
+echo "Suresh:: Hello Ramesh, I am fine. How are you?"
+echo "Ramesh:: What are you learning recently?"
+echo "Suresh:: I am learning Shell scripting Ramesh"
