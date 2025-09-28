4e9289fa69c740a35383d08d30bb2883fb60a73a
arunsagar3677-Devops <you@example.com>
2025-09-28 14:41:36 +0530

    sss

--- 03-variable.sh
+++ 03-variable.sh
@@ -1,8 +1,8 @@
 #!/bin/bash
 
 #No space before after equal
-PERSON1=John
-PERSON2=Raheem
+PERSON1=Dravid
+PERSON2=Arun
 
 echo "$PERSON1:: Hi $PERSON2, How are you?"
 echo "$PERSON2:: Hello $PERSON1, I am fine. How are you?"
